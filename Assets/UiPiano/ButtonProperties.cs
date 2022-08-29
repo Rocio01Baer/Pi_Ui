0c36fffdca0dfa2480d6849d7d79c391e39c8af4
Rocio Ayala <eli19baer@gmail.com>
2022-08-29 14:19:40 -0400

    First commit

---
+++ Assets/UiPiano/ButtonProperties.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class ButtonProperties : MonoBehaviour  {
+
+	public Image imageButton;
+
+	void Start()
+	{
+		imageButton = GetComponent<Image>();
+	}
+
+	public void ColorEnterPoint()
+	{
+		imageButton.color = Color.cyan;
+	}
+
+	public void ColorPointEnterSharp()
+	{
+		imageButton.color = Color.gray;
+	}
+
+	public void ColorPointerOut()
+	{
+		imageButton.color = Color.white;
+	}
+
+	public void ColorPointOutSharp()
+	{
+		imageButton.color = Color.black;
+	}
+	
+}
